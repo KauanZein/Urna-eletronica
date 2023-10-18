bace0f5294fa78e73e26faa0b84e3db3d75ee9ce
KauanZein <kauanzein@gmail.com>
2023-10-18 12:01:13 -0300

    feat: adicionando porcentagem

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,15 +1,21 @@
 function urnaEletronica() {
 
-console.log('Insira seu voto');
+    console.log('Votação')
     
-    let opcao;
-    let nome1;
-    let nome2;
-    let nome3;
+    console.log('Insira seu voto');
+    
+    let opcao = 0;
+    let contador = 0;
+    let nomeC1;
+    let nomeC2;
+    let nomeC3;
 
-    nome1 = prompt('Digite o nome do candidato');
-    nome2 = prompt('Digite o nome do candidato');
-    nome3 = prompt('Digite o nome do candidato');
+    nomeC1 = prompt('Digite o nome do candidato:');
+    console.log(nomeC1);
+    nomeC2 = prompt('Digite o nome do candidato:');
+    console.log(nomeC2);
+    nomeC3 = prompt('Digite o nome do candidato:');
+    console.log(nomeC3);
     
     let candidato1 = 0;
     let candidato2 = 0;
@@ -40,10 +46,24 @@ console.log('Insira seu voto');
 
     } while (opcao !== 0);
 
-        contador--;
+        contador = contador - 1;
+        console.log('Contagem:', contador);
+        console.log('total de votos do', nomeC1, candidato1);
+        console.log('total de votos do', nomeC2, candidato2);
+        console.log('total de votos do', nomeC3, candidato3);
+        console.log('total de votos brancos', brancos);
+        console.log('total de votos nulos', nulos);
 
-    console.log('Contagem:', contador);
+        const totalvotos = (candidato1 + candidato2 + candidato3 + brancos + nulos);
+        console.log('Total de votos', totalvotos);
 
+        console.log('percentual de votos candidato 1', ((candidato1 / totalvotos * 100).toFixed(2)) + '%');
+        console.log('percentual de votos candidato 2', ((candidato2 / totalvotos * 100).toFixed(2)) + '%');
+        console.log('percentual de votos candidato 3', ((candidato3 / totalvotos * 100).toFixed(2)) + '%');
+        console.log('percentual de votos em branco', ((brancos / totalvotos * 100).toFixed(2)) + '%');
+        console.log('percentual de votos nulos', ((nulos / totalvotos * 100).toFixed(2)) + '%');
+        
+        
 }
 
 urnaEletronica()
